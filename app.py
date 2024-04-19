b641e1a8e6345451ca8c45e6768d7d6830bd22ba
InnaBoby <146350138+InnaBoby@users.noreply.github.com>
2024-04-19 11:41:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from feat_imp import *
 
 st.header('Feature Importance')
 
-data = pd.read_csv('School_Attendance_by_Student_Group.csv')
+data = pd.read_csv('climate_change_indicators.csv')
 if st.button('Показать данные'):
     st.table(data)
 
